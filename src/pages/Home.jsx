d4b290de6381c72ad9f691e9786d53e406dc5ce0
yongmin <vmflxu@gmail.com>
2023-03-17 20:04:46 +0900

    버튼스타일규격화

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { PageContainer } from '../variables/styleStore'
+import { S_BUTTON } from '../variables/uiVariables'
 
 function Home() {
     return (
         <PageContainer>
             page: Home
+            <BUTTON></BUTTON>
         </PageContainer>
     )
 }
