3142b0d95c3adbf9b4ef17b1e90dbded73d1d2d8
SsongCh94 <thddkwl94@gmail.com>
2023-03-18 01:08:08 +0900

    detail 끝나가나

--- src/pages/BbsDetail.jsx
+++ src/pages/BbsDetail.jsx
@@ -25,6 +25,12 @@ function BbsDetail() {
           <CommentsInput type="text" placeholder="댓글을 입력하세요" />
           <ButtonSmall>댓글 달기</ButtonSmall>
         </CommentsArea>
+        <CommentsList></CommentsList>
+        <Test>
+          {" "}
+          <ButtonMiddle />
+          <ButtonMiddle />
+        </Test>
       </DetailArea>
     </PageContainer>
   );
@@ -33,25 +39,27 @@ function BbsDetail() {
 const DetailArea = styled.div`
   background-color: aqua;
   width: 100%;
-  height: 85vh;
+  height: 100%;
 
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 10px;
+
+  overflow-y: scroll;
 `;
 
 const PhotoArea = styled.div`
   background-color: beige;
   width: 70%;
-  height: 30%;
+  height: 400px;
 `;
 
 const InfoArea = styled.div`
   background-color: azure;
   width: 70%;
-  height: 5%;
+  height: 40px;
 
   display: flex;
   justify-content: space-evenly;
@@ -63,20 +71,60 @@ const InfoArea = styled.div`
 const Contents = styled.div`
   background-color: beige;
   width: 70%;
-  height: 20%;
+  height: 200px;
 `;
 
 const CommentsArea = styled.div`
   background-color: beige;
-  width: 70%;
+  width: 60%;
   height: 5%;
 
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
 `;
 
 const CommentsInput = styled.input`
   width: 70%;
+`;
+
+const CommentsList = styled.div`
+  background-color: beige;
+  width: 60%;
+  height: 699px;
+`;
+
+const Test = styled.div`
+  background-color: black;
+  width: 200px;
+  height: 200px;
+
+  position: fixed;
+
+  right: 0;
+  bottom: 100px;
+`;
+
+// TODO: 버튼 가져왔음!
+
+const COLOR_1 = "37306B";
+const COLOR_2 = "66347F";
+const COLOR_3 = "9E4784";
+const COLOR_4 = "D27685";
+
+const ButtonMiddle = styled.button`
+  cursor: pointer;
+  padding: 10px 20px;
+  width: auto;
+  height: auto;
+  color: #${COLOR_4};
+  border: 1px solid #${COLOR_4};
+  background-color: #${COLOR_1};
+  &:hover {
+    color: #${COLOR_3};
+    border: 1px solid #${COLOR_3};
+    background-color: #${COLOR_2};
+  }
+  ${({ others }) => others};
 `;
 
 export default BbsDetail;
